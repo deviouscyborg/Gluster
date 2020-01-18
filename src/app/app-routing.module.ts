f30e2ad45c747109623cdddc7db6f6e883f5be25
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-01-18 14:49:24 +0530

    added news component and updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
 import {HomeComponent} from '~/app/home/home.component';
+import {NewsComponent} from '~/app/news/news.component';
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
+    { path: "news", component: NewsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import {HomeComponent} from '~/app/home/home.component';
+import {NewsComponent} from '~/app/news/news.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -22,6 +23,7 @@ import {HomeComponent} from '~/app/home/home.component';
     declarations: [
         AppComponent,
         HomeComponent,
+        NewsComponent
     ],
     providers: [],
     schemas: [
