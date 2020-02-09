5cff03b78941a7e0fa492c94033573729276a8cf
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-02-10 00:01:51 +0530

    configured search component and some minor changes in categories and favourites component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchBar } from "tns-core-modules/ui/search-bar";
+import {GamesService} from '~/app/games/games.service';
+import {Game1} from '~/app/games/games.component';
 
 @Component({
   selector: 'ns-search',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+    searchPhrase: string;
+    private isLoading= false;
+    games: Game1[];
 
-  constructor() { }
+
+  constructor(private gamesService: GamesService) { }
 
   ngOnInit() {
   }
 
+    onSubmit(args) {
+        this.isLoading = true;
+        const searchBar = args.object as SearchBar;
+        this.searchGames(searchBar.text);
+        console.log(`Searching for ${searchBar.text}`);
+    }
+
+    onTextChanged(args) {
+        const searchBar = args.object as SearchBar;
+        console.log(`Input changed! New value: ${searchBar.text}`);
+    }
+
+    onClear(args) {
+        const searchBar = args.object as SearchBar;
+        this.searchGames(searchBar.text);
+        console.log(`Clear event raised`);
+    }
+
+    searchGames(title: string) {
+        this.gamesService
+            .getGames("", "", ""+title)
+            .subscribe( (response: Game1[]) => {
+                this.isLoading = false;
+                this.games = response;
+            }, error => {
+                this.isLoading = false;
+                console.log(error);
+            });
+    }
+
+    playGame(game) {
+
+    }
 }
