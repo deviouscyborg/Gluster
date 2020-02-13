f3a47940c019b54130db028eae153b93e0e6a5e4
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-02-14 00:05:29 +0530

    changed name of app, add NS Image cache plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import {initializeOnAngular} from 'nativescript-image-cache';
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor () {
+        initializeOnAngular();
+    }
+}
