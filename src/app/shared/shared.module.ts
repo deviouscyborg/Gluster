9e5d9b12d773b0ef37fd6823a7359c9683e58ab1
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-02-15 10:57:25 +0530

    added modal and error component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { NoDataImgComponent } from './no-data-img/no-data-img.component';
+import { ModalComponent } from './modal/modal.component';
+import { ErrorComponent } from './error/error.component';
 
 
 
 @NgModule({
-  declarations: [NoDataImgComponent],
+  declarations: [NoDataImgComponent, ModalComponent, ErrorComponent],
   imports: [
     NativeScriptCommonModule
   ],
