065d2e1342778b508f6fcd465a6e6817747e4118
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-02-17 00:51:07 +0530

    added side drawer and action bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {isAndroid, Page} from "tns-core-modules/ui/page";
 import {Router} from '@angular/router';
 import * as application from 'tns-core-modules/application';
 import {AndroidApplication} from 'tns-core-modules/application';
 import {AndroidActivityBackPressedEventData} from 'tns-core-modules/application';
 import * as Toast from 'nativescript-toast';
+import {RadSideDrawer} from 'nativescript-ui-sidedrawer';
+import { isIOS } from "tns-core-modules/platform";
 
 @Component({
     selector: 'ns-home',
@@ -13,14 +15,46 @@ import * as Toast from 'nativescript-toast';
 })
 export class HomeComponent implements OnInit {
     tries: number = 0;
+    @ViewChild("rsd", {static:false}) rSideDrawer: ElementRef;
+    sideDrawerOptions: string[] = [
+        "Share",
+        "Rate Us"
+    ];
 
     constructor(private page: Page, private router: Router) {}
 
     ngOnInit() {
-        this.page.actionBarHidden = true;
+        // this.page.actionBarHidden = true;
         if(isAndroid) {
             this.backButtonPressed();
         }
+    }
+    isIOS(): boolean {
+        return isIOS;
+    }
+
+    isAndroid(): boolean {
+        return isAndroid;
+    }
+    drawerLoaded(args) {
+        let drawer = <RadSideDrawer>args.object;
+
+        if (isIOS) {
+            // if your menu is drawn 'below' the hostview, do this:
+            //drawer.ios.defaultSideDrawer.style.shadowMode = 1; // TKSideDrawerShadowMode.Hostview;
+            drawer.ios.defaultSideDrawer.style.shadowMode = 2; // TKSideDrawerShadowMode.SideDrawer;
+            // if you have shadowMode = 2, then you can add a little dim to the lower layer to add some depth. Keep it subtle though:
+            drawer.ios.defaultSideDrawer.style.dimOpacity = 0.12;
+            // then tweak the shadow to your liking:
+            drawer.ios.defaultSideDrawer.style.shadowOpacity = 0.75; // 0-1, higher is darker
+            drawer.ios.defaultSideDrawer.style.shadowRadius = 5; // higher is more spread
+            // bonus feature: control the menu animation speed (in seconds)
+            drawer.ios.defaultSideDrawer.transitionDuration = 0.25;
+        }
+    }
+
+    onOpenDrawerTap(): void {
+        this.rSideDrawer.nativeElement.toggleDrawerState();
     }
 
     backButtonPressed() {
@@ -33,5 +67,9 @@ export class HomeComponent implements OnInit {
                 }, 2000);
             }
         });
+    }
+
+    sideDrawerOptionTap(event) {
+
     }
 }
