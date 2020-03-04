c9e0c238dd95421b8c57ada9fffaa04d5fda45fd
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-03-04 13:28:19 +0530

    configured push notifications (tested by sending notifications using postman)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {initializeOnAngular} from 'nativescript-image-cache';
+import * as firebase from 'nativescript-plugin-firebase';
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     constructor () {
         initializeOnAngular();
     }
+    ngOnInit() {
+    firebase.init({
+                      showNotifications: true,
+                      showNotificationsWhenInForeground: true,
+
+                      onPushTokenReceivedCallback: (token) => {
+    console.log('[Firebase] onPushTokenReceivedCallback:', { token });
+},
+
+onMessageReceivedCallback: (message: firebase.Message) => {
+    console.log('[Firebase] onMessageReceivedCallback:', { message });
+}
+})
+.then(() => {
+    console.log('[Firebase] Initialized');
+})
+    .catch(error => {
+        console.log('[Firebase] Initialize', { error });
+    });
+
+}
 }
