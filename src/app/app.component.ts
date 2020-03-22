2ae2ad03001973c7563e3ab8279e00fc74d8522c
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-03-22 18:22:03 +0530

    added functions for admob

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import * as firebase from 'nativescript-plugin-firebase';
 import { initializeOnAngular } from 'nativescript-image-cache';
+import * as Admob from "nativescript-admob";
 
 @Component({
     selector: "ns-app",
@@ -8,9 +9,53 @@ import { initializeOnAngular } from 'nativescript-image-cache';
     templateUrl: "./app.component.html"
 })
 export class AppComponent implements OnInit{
+    private androidBannerId: string = "ca-app-pub-4500169197993541/8614481600";
+    private androidInterstitialId: string = "ca-app-pub-4500169197993541/7109828246";
+    // private iosBannerId: string = "ca-app-pub-RRRR/TTTT";
+    // private iosInterstitialId: string = "ca-app-pub-GGGG/HHHH";
+
     constructor () {
         initializeOnAngular();
     }
+
+    public createBanner() {
+        Admob.createBanner({
+            testing: true,
+            size: Admob.AD_SIZE.SMART_BANNER,
+            // iosBannerId: this.iosBannerId,
+            androidBannerId: this.androidBannerId,
+            // iosTestDeviceIds: ["yourTestDeviceUDIDs"],
+            margins: {
+                bottom: 0
+            }
+        }).then(function() {
+            console.log("admob createBanner done");
+        }, function(error) {
+            console.log("admob createBanner error: " + error);
+        });
+    }
+
+    public hideBanner() {
+        Admob.hideBanner().then(function() {
+            console.log("admob hideBanner done");
+        }, function(error) {
+            console.log("admob hideBanner error: " + error);
+        });
+    }
+
+    public createInterstitial() {
+        Admob.createInterstitial({
+            testing: true,
+            // iosInterstitialId: this.iosInterstitialId,
+            androidInterstitialId: this.androidInterstitialId,
+            // iosTestDeviceIds: ["yourTestDeviceUDIDs"]
+        }).then(function() {
+            console.log("admob createInterstitial done");
+        }, function(error) {
+            console.log("admob createInterstitial error: " + error);
+        });
+    }
+
     ngOnInit() {
     firebase.init({
                       showNotifications: true,
